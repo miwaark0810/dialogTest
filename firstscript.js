02286c8e4fcd06231fb9968d7c0532a5821ef403
miwaark0810 <73152294+miwaark0810@users.noreply.github.com>
2023-02-09 18:41:26 +0900

    Create firstscript.js

---
+++ firstscript.js
@@ -0,0 +1,15 @@
+const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) );
+const aFunc = async function( ){
+
+    console.log("1回目")
+
+    await sleep( 3000 );
+
+    console.log("2回目")
+
+    await sleep( 1000 );
+  
+    console.log("終わり")
+
+}
+aFunc;
