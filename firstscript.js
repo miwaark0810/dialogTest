c05d2ed84f4fb0c392ca714bd6ba50b93183455e
miwaark0810 <73152294+miwaark0810@users.noreply.github.com>
2023-02-09 18:54:00 +0900

    Update firstscript.js

--- firstscript.js
+++ firstscript.js
@@ -1,15 +1,16 @@
-const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) );
-const aFunc = async function(){
+console.log("テスト")
+// const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) );
+// const aFunc = async function(){
 
-    console.log("1回目")
+//     console.log("1回目")
 
-    await sleep( 3000 );
+//     await sleep( 3000 );
 
-    console.log("2回目")
+//     console.log("2回目")
 
-    await sleep( 1000 );
+//     await sleep( 1000 );
   
-    console.log("終わり")
+//     console.log("終わり")
 
-}
-console.log(aFunc);
+// }
+// console.log(aFunc);
